7ebb3f09e81e961860cd5fd7d33a70076c162546
Laurentiu Tibea <laurentiutibea@gmail.com>
2018-11-12 14:32:28 +0200

    update

--- html5/cam.js
+++ html5/cam.js
@@ -14,19 +14,3 @@ function video(){
             alert("Cannot read camera");
         });
 }
-
-/* eslint-disable linebreak-style */
-document.getElementById("id_business_version").innerHTML = "Business version: 2018.11.12.0";
-let v = document.getElementById("id_video");
-v.addEventListener("click", video);
-
-function video(){
-    let c = {audio: true, video: true};
-    navigator.mediaDevices.getUserMedia(c)
-        .then( (e) => {
-            v.srcObject = e;
-        })
-        .catch( () => {
-            alert("Cannot read camera");
-        });
-}
