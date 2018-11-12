7415c1831b7646e6d4f8bc8d6f67b9214ffcf4ab
Laurentiu Tibea <laurentiutibea@gmail.com>
2018-11-12 21:43:31 +0200

    update camera

--- html5/cam_es6.js
+++ html5/cam_es6.js
@@ -2,34 +2,37 @@
 document.getElementById("id_business_version").innerHTML = "Business version: ES6 2018.11.12.5";
 document.getElementById("id_button").addEventListener("click", video);
 document.getElementById("id_change").addEventListener("click", video);
+document.getElementById("id_stop").addEventListener("click", video_stop);
 document.getElementById("id_video").addEventListener("touchstart", on_touch);
 document.getElementById("id_video").addEventListener("mousedown", on_touch);
 
+let c = {};
+let front = false;
 function video(){
-    let frontCam = true;
-    let c = {};
-    if(frontCam === true) {
-        c = {audio: true, video: {facingMode: "environment"}};
-        frontCam=false;
-    }
-    else {
-        c = {audio: true, video: {facingMode: "user"}};
-        frontCam=true;
-    }
+    console.log("inceput");
+
+    c = {audio: true, video: {facingMode: (front? "user" : "environment")}};
+    console.log("ELSE");
     navigator.mediaDevices.getUserMedia(c)
         .then( (e) => {
             document.getElementById("id_video").srcObject = e;
+            console.log("then");
         })
         .catch( () => {
             alert("Cannot read camera");
         });
+    front = !front;
+}
+
+function video_stop(){
+    c = {audio: false, video: false};
 }
 
 function on_touch()
 {
-    var canvas = document.getElementById("id_canvas");
-    var context = canvas.getContext("2d");
-    var video = document.getElementById("id_video");
+    let canvas = document.getElementById("id_canvas");
+    let context = canvas.getContext("2d");
+    let video = document.getElementById("id_video");
 
     context.drawImage(video, 0, 0);
 }
