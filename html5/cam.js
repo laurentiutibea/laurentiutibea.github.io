269498492d41dce26f4517fba919336f45bc7b69
Laurentiu Tibea <laurentiutibea@gmail.com>
2018-11-12 14:36:21 +0200

    update

--- html5/cam.js
+++ html5/cam.js
@@ -1,5 +1,5 @@
 /* eslint-disable linebreak-style */
-document.getElementById("id_business_version").innerHTML = "Business version: 2018.11.12.1";
+/*document.getElementById("id_business_version").innerHTML = "Business version: 2018.11.12.1";
 let v = document.getElementById("id_button");
 v.addEventListener("click", video);
 
@@ -13,4 +13,35 @@ function video(){
         .catch( () => {
             alert("Cannot read camera");
         });
+}*/
+
+document.getElementById("id_business_version").innerHTML = "Business version: 2018.11.12.3";
+document.getElementById("id_button").addEventListener("click", start);
+document.getElementById("id_video").addEventListener("touchstart", on_touch);
+document.getElementById("id_video").addEventListener("mousedown", on_touch);
+
+//------------------------------
+function on_success(e)
+{
+    document.getElementById("id_video").srcObject = e;
+}
+//------------------------------
+function on_failure(e)
+{
+    alert("Eroare conectare");
+}
+//------------------------------
+function start()
+{
+    var c = {audio: true, video: {facingMode: "user"}};
+    navigator.mediaDevices.getUserMedia(c).then(on_success).catch(on_failure);
+}
+//------------------------------
+function on_touch(e)
+{
+    var canvas = document.getElementById("id_canvas");
+    var context = canvas.getContext("2d");
+    var video = document.getElementById("id_video");
+
+    context.drawImage(video, 0, 0);
 }
