c7cb3677d37092c8dd52119f91f885eedd93f1da
Laurentiu Tibea <laurentiutibea@gmail.com>
2018-11-12 18:46:08 +0200

    update

--- html5/cam_es6.js
+++ html5/cam_es6.js
@@ -1,11 +1,23 @@
 /* eslint-disable linebreak-style */
 document.getElementById("id_business_version").innerHTML = "Business version: ES6 2018.11.12.5";
 document.getElementById("id_button").addEventListener("click", video);
+document.getElementById("id_change").addEventListener("click", video_front);
 document.getElementById("id_video").addEventListener("touchstart", on_touch);
 document.getElementById("id_video").addEventListener("mousedown", on_touch);
 
 function video(){
     let c = {audio: true, video: {facingMode: "environment"}};
+    navigator.mediaDevices.getUserMedia(c)
+        .then( (e) => {
+            document.getElementById("id_video").srcObject = e;
+        })
+        .catch( () => {
+            alert("Cannot read camera");
+        });
+}
+
+function video_front(){
+    let c = {audio: true, video: {facingMode: "user"}};
     navigator.mediaDevices.getUserMedia(c)
         .then( (e) => {
             document.getElementById("id_video").srcObject = e;
