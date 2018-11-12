c73ec6fd6a5bd1213f6212f2c7ae0e3f84262423
Laurentiu Tibea <laurentiutibea@gmail.com>
2018-11-12 14:30:36 +0200

    update

--- html5/cam.js
+++ html5/cam.js
@@ -1,4 +1,20 @@
-<<<<<<< HEAD
+/* eslint-disable linebreak-style */
+document.getElementById("id_business_version").innerHTML = "Business version: 2018.11.12.1";
+let v = document.getElementById("id_button");
+v.addEventListener("click", video);
+
+function video(){
+    let c = document.getElementById("id_video");
+        c = {audio: true, video: true};
+    navigator.mediaDevices.getUserMedia(c)
+        .then( (e) => {
+            v.srcObject = e;
+        })
+        .catch( () => {
+            alert("Cannot read camera");
+        });
+}
+
 /* eslint-disable linebreak-style */
 document.getElementById("id_business_version").innerHTML = "Business version: 2018.11.12.0";
 let v = document.getElementById("id_video");
@@ -14,24 +30,3 @@ function video(){
             alert("Cannot read camera");
         });
 }
-
-
-=======
-/* eslint-disable linebreak-style */
-document.getElementById("id_business_version").innerHTML = "Business version: 2018.11.12.0";
-let v = document.getElementById("id_video");
-v.addEventListener("click", video);
-
-function video(){
-    let c = {audio: true, video: true};
-    navigator.mediaDevices.getUserMedia(c)
-        .then( (e) => {
-            v.srcObject = e;
-        })
-        .catch( () => {
-            alert("Cannot read camera");
-        });
-}
-
-
->>>>>>> 9fa05b4eda37881117d336628975969bfa464e70
