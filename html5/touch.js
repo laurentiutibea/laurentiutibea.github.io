c4824bbd295ec5c47a218e1b8c6eab4d2bef96fe
Laurentiu <laurentiutibea@gmail.com>
2018-11-26 14:43:56 +0200

    touchmove

--- html5/touch.js
+++ html5/touch.js
@@ -23,10 +23,14 @@ canvas.addEventListener("touchmove", (e) => {
     for(let i = 0; i < e.changedTouches.length; i++){
         let context = canvas.getContext("2d");
         context.beginPath();
+        context.lineWidth = 20;
         context.moveTo(last_pos.x - canvas_bounding_rect.left,last_pos.y - canvas_bounding_rect.top);
         context.lineTo(
             e.changedTouches[i].pageX - canvas_bounding_rect.left,
             e.changedTouches[i].pageY - canvas_bounding_rect.top,);
+        context.stroke();
+        context.beginPath();
+        context.lineWidth = 1;
         context.arc(
             e.changedTouches[i].pageX - canvas_bounding_rect.left,
             e.changedTouches[i].pageY - canvas_bounding_rect.top,
