63991514ec4065f9346241681d5d34e31e89d55f
Laurentiu <laurentiutibea@gmail.com>
2018-11-26 14:26:41 +0200

    touchmove

--- html5/touch.js
+++ html5/touch.js
@@ -1,7 +1,20 @@
-document.getElementById("id_business_version").innerHTML = "Business version: 2018.11.26.1";
+document.getElementById("id_business_version").innerHTML = "Business version: 2018.11.26.2";
 let canvas  = document.getElementById("id_canvas");
 let canvas_bounding_rect = canvas.getBoundingClientRect();
+
 canvas.addEventListener("touchstart", (e) => {
+    for(let i = 0; i < e.changedTouches.length; i++){
+        let context = canvas.getContext("2d");
+        context.beginPath();
+        context.arc(
+            e.changedTouches[i].pageX - canvas_bounding_rect.left,
+            e.changedTouches[i].pageY - canvas_bounding_rect.top,
+            10, 0, 2*Math.PI);
+        context.stroke();
+    }
+});
+
+canvas.addEventListener("touchmove", (e) => {
     for(let i = 0; i < e.changedTouches.length; i++){
         let context = canvas.getContext("2d");
         context.beginPath();
