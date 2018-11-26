c5b6c5833e22dbe468ec2bd35a0eef8ff39c8035
Laurentiu <laurentiutibea@gmail.com>
2018-11-26 14:21:05 +0200

    touch

--- html5/touch.js
+++ html5/touch.js
@@ -1,11 +1,14 @@
-document.getElementById("id_business_version").innerHTML = "Business version: 2018.11.26.0";
-
+document.getElementById("id_business_version").innerHTML = "Business version: 2018.11.26.1";
 let canvas  = document.getElementById("id_canvas");
+let canvas_bounding_rect = canvas.getBoundingClientRect();
 canvas.addEventListener("touchstart", (e) => {
     for(let i = 0; i < e.changedTouches.length; i++){
         let context = canvas.getContext("2d");
         context.beginPath();
-        context.arc(e.changedTouches[i].pageX, e.changedTouches[i].pageY, 10, 0, 2*Math.PI);
+        context.arc(
+            e.changedTouches[i].pageX - canvas_bounding_rect.left,
+            e.changedTouches[i].pageY - canvas_bounding_rect.top,
+            10, 0, 2*Math.PI);
         context.stroke();
     }
 });
