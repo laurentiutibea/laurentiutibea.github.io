9d8a78a65d3f0950f6cf6329726d3f9ef3f3b7bf
Laurentiu <laurentiutibea@gmail.com>
2018-12-04 01:45:29 +0200

    svg

--- html5/svg.js
+++ html5/svg.js
@@ -1,15 +1,14 @@
-document.getElementById("id_business_version").innerHTML = "Business version: 2018.12.03.0";
-window.addEventListener("deviceorientation", on_device_orientation);
+document.getElementById("id_business_version").innerHTML = "Business version: 2018.12.03.1";
 
-function on_device_orientation(e){
+window.addEventListener("deviceorientation",(e) => {
     let svg = document.getElementById("id_svg");
-    let cerc = document.getElementById("id_cerc");
+    let circle = document.getElementById("id_circle");
 
     let R = 20;
 
     let svg_height = svg.getAttribute("height");
     let svg_width = svg.getAttribute("width");
 
-    cerc.setAttribute("cx",  svg_width / 2 + e.gamma / 90 * (svg_width / 2 - R));
-    cerc.setAttribute("cy", svg_height / 2 + + e.beta / 90 * (svg_height / 2 - R));
-}
+    circle.setAttribute("cx",  svg_width / 2 + e.gamma / 90 * (svg_width / 2 - R));
+    circle.setAttribute("cy", svg_height / 2 + + e.beta / 90 * (svg_height / 2 - R));
+});
